f4c150ec630efbedfc64a598dc837607d777bb38
Chris Humboldt <chrismodem@gmail.com>
2014-05-22 07:31:26 +0200

    Tons of changes.

---
+++ gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+
+	// Load NPM tasks
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', ['watch']);
+	
+	// Initialize config
+	grunt.initConfig({
+		// Package
+		pkg: grunt.file.readJSON('package.json'),
+		// SASS
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed'
+				},
+				files: {
+					'css/formplate.css': 'sass/formplate.scss',
+					'css/demo.css': 'sass/demo.scss'
+				}
+			}
+		}, 
+		// Uglify
+		uglify: {
+			my_target: {
+				files: {
+					'js/min/jquery-v1.10.2.min.js'			: ['js/jquery-v1.10.2.js'],
+					'js/min/formplate.min.js'				: ['js/formplate.js']
+				}
+			}
+		},
+		// Watch
+		watch: {
+			// CSS
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			},
+			// Scripts
+			scripts: {
+				files: 'js/*.js',
+				tasks: ['uglify']
+			},
+			// Live reload
+			options: {
+				livereload: true,
+		    } 
+		} 
+	});
+}
