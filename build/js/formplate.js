91e2f43ec519e19ae1431fd1b93a87d710e1d06c
Chris Humboldt <chrismodem@gmail.com>
2016-11-01 21:19:04 +0200

    Refactoring JS almost done.

--- build/js/formplate.js
+++ build/js/formplate.js
@@ -4,273 +4,298 @@
  * Author: Chris Humboldt
 **/
 
-// Table of contents
-// Defaults
-// Tools
-// Variables
-
-// Defaults
-var $formplateDefault = {
-	selector: '.formplate',
-	colour: 'blue',
-	style: 'line',
-	label: 'normal'
-};
-
-function formplate($userOptions) {
-	// Tools
-	var tool = function(document) {
-		// Elements
-		var $toolEl = {
-			body: document.getElementsByTagName('body')[0],
-			html: document.getElementsByTagName('html')[0]
+// Webplate tools module extension
+var Web = (function (Web) {
+	// Basic checks
+	if (!Web.exists) {
+		var exists = function (check) {
+			return (check === null || check === false || typeof (check) == 'undefined') ? false : true;
 		};
-
-		// Functions
-		var classAdd = function($element, $class) {
-			if (exists($element)) {
-				if (typeof $class === 'object') {
-					for (var $i = 0, $len = $class.length; $i < $len; $i++) {
-						classAddExecute($element, $class[$i]);
+		Web.exists = exists;
+	}
+	if (!Web.has) {
+		var has = {
+			spaces: function (check) {
+				return /\s/.test(check);
+			},
+			class: function (element, className) {
+				return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
+			}
+		};
+		Web.has = has;
+	}
+	if (!Web.is) {
+		var is = {
+			touch: function () {
+				return 'ontouchstart' in window || 'onmsgesturechange' in window;
+			}
+		};
+		Web.is = is;
+	}
+	// Classes
+	if (!Web.class) {
+		var classMethods = {
+			add: function (element, className) {
+				if (exists(element)) {
+					if (typeof className === 'object') {
+						for (var i = 0, len = className.length; i < len; i++) {
+							classMethods.addExecute(element, className[i]);
+						}
+					} else if (has.spaces(className)) {
+						var classes = className.split(' ');
+						for (var i = 0, len = classes.length; i < len; i++) {
+							classMethods.addExecute(element, classes[i]);
+						}
+					} else {
+						classMethods.addExecute(element, className);
 					}
-				} else if (hasWhiteSpace($class)) {
-					var $classes = $class.split(' ');
-					for (var $i = 0, $len = $classes.length; $i < $len; $i++) {
-						classAddExecute($element, $classes[$i]);
+				}
+			},
+			addExecute: function (element, className) {
+				var crtClass = element.className;
+				if (crtClass.match(new RegExp('\\b' + className + '\\b', 'g')) === null) {
+					element.className = crtClass === '' ? className : crtClass + ' ' + className;
+				}
+			},
+			clear: function (element) {
+				if (exists(element)) {
+					element.removeAttribute('class');
+				}
+			},
+			remove: function (element, className) {
+				if (exists(element)) {
+					if (typeof className === 'object') {
+						for (var i = className.length - 1; i >= 0; i--) {
+							classMethods.removeExecute(element, className[i]);
+						}
+					} else if (has.spaces(className)) {
+						var classes = className.split(' ');
+						for (var i = 0, len = classes.length; i < len; i++) {
+							classMethods.removeExecute(element, classes[i]);
+						}
+					} else {
+						classMethods.removeExecute(element, className);
 					}
+				}
+			},
+			removeExecute: function (element, className) {
+				if (element.className.indexOf(className) > -1) {
+					element.className = element.className.split(' ').filter(function (val) {
+						return val != className;
+					}).toString().replace(/,/g, ' ');
+					if (element.className === '') {
+						classMethods.clear(element);
+					}
+				}
+			}
+		};
+		Web.class = classMethods;
+	}
+	// Gets
+	if (!Web.get) {
+		var get = {
+			index: function (node) {
+				return [].indexOf.call(node.parentNode.children, node);
+			},
+			extension: function (file) {
+				return file.split('.').pop().toLowerCase();
+			},
+			integers: function (string) {
+				return string.replace(/^\D+ /g, '').replace(/ /g, '');
+			}
+		};
+		Web.get = get;
+	}
+	// Development
+	if (!Web.log) {
+		var log = function (text) {
+			if (window && window.console) {
+				console.log(text);
+			}
+		};
+		Web.log = log;
+	}
+	// DOM
+	if (!Web.dom) {
+		Web.dom = {};
+	}
+	if (!Web.dom.html) {
+		Web.dom.html = document.getElementsByTagName('html')[0];
+	}
+	// Events
+	if (!Web.event) {
+		var eventMethods = {
+			add: function (elem, type, eventHandle) {
+				if (elem == null || typeof (elem) == 'undefined') return;
+				if (elem.addEventListener) {
+					elem.addEventListener(type, eventHandle, false);
+				} else if (elem.attachEvent) {
+					elem.attachEvent('on' + type, eventHandle);
 				} else {
-					classAddExecute($element, $class);
+					elem['on' + type] = eventHandle;
 				}
-			}
-		};
-		var classAddExecute = function($element, $class) {
-			var $crtClass = $element.className;
-			if ($crtClass.match(new RegExp('\\b' + $class + '\\b', 'g')) === null) {
-				$element.className = $crtClass === '' ? $class : $crtClass + ' ' + $class;
-			}
-		};
-		var classClear = function($element) {
-			if (exists($element)) {
-				$element.removeAttribute('class');
-			}
-		};
-		var classRemove = function($element, $class) {
-			if (exists($element)) {
-				if (typeof $class === 'object') {
-					for (var $i = $class.length - 1; $i >= 0; $i--) {
-						classRemoveExecute($element, $class[$i]);
-					}
-				} else if (hasWhiteSpace($class)) {
-					var $classes = $class.split(' ');
-					for (var $i = 0, $len = $classes.length; $i < $len; $i++) {
-						classRemoveExecute($element, $classes[$i]);
-					}
+			},
+			remove: function (elem, type, eventHandle) {
+				if (elem == null || typeof (elem) == 'undefined') return;
+				if (elem.removeEventListener) {
+					elem.removeEventListener(type, eventHandle, false);
+				} else if (elem.detachEvent) {
+					elem.detachEvent('on' + type, eventHandle);
 				} else {
-					classRemoveExecute($element, $class);
+					elem['on' + type] = eventHandle;
 				}
 			}
 		};
-		var classRemoveExecute = function($element, $class) {
-			if ($element.className.indexOf($class) > -1) {
-				$element.className = $element.className.split(' ').filter(function($val) {
-					return $val != $class;
-				}).toString().replace(/,/g, ' ');
-				if ($element.className === '') {
-					classClear($element);
-				}
-			}
-		};
-		var clone = function ($elm) {
-			var $clone;
-
-			switch(typeof $elm) {
-				case 'object':
-					if ($elm instanceof Array) {
-						return JSON.parse(JSON.stringify($elm));
-					}
-					break;
-				default:
+		Web.event = eventMethods;
+	}
+	// Helpers
+	if (!Web.helper) {
+		var helper = {
+			setDefault: function (setValue, defaultValue) {
+				if (typeof setValue == 'undefined' && typeof defaultValue == 'undefined') {
 					return false;
-					break;
+				} else if (typeof setValue != 'undefined' && typeof defaultValue == 'undefined') {
+					return setValue;
+				} else if (typeof setValue === typeof defaultValue) {
+					return setValue;
+				} else {
+					return defaultValue;
+				}
 			}
 		};
-		var eventAdd = function($elem, $type, $eventHandle) {
-			if ($elem == null || typeof($elem) == 'undefined') return;
-			if ($elem.addEventListener) {
-				$elem.addEventListener($type, $eventHandle, false);
-			} else if ($elem.attachEvent) {
-				$elem.attachEvent("on" + $type, $eventHandle);
-			} else {
-				$elem["on" + $type] = $eventHandle;
-			}
-		};
-		var exists = function($element) {
-			return ($element === null || typeof($element) === undefined) ? false : true;
-		};
-		var hasClass = function($element, $class) {
-			return (' ' + $element.className + ' ').indexOf(' ' + $class + ' ') > -1;
-		};
-		var hasWhiteSpace = function($check) {
-			return /\s/.test($check);
-		};
-		var isTouch = function() {
-			return 'ontouchstart' in window || 'onmsgesturechange' in window;
-		};
-
-		return {
-			classAdd: classAdd,
-			classClear: classClear,
-			classRemove: classRemove,
-			clone: clone,
-			element: $toolEl,
-			eventAdd: eventAdd,
-			hasClass: hasClass,
-			isTouch: isTouch
-		}
-	}(document);
-
-	// Variables
-	var $self = this;
-	$self.options = {
-		selector: ($userOptions && $userOptions.selector) ? $userOptions.selector : $formplateDefault.selector,
-		colour: ($userOptions && $userOptions.colour) ? $userOptions.colour : $formplateDefault.colour,
-		style: ($userOptions && $userOptions.style) ? $userOptions.style : $formplateDefault.style,
-		label: ($userOptions && $userOptions.label) ? $userOptions.label : $formplateDefault.label,
+		Web.helper = helper;
 	}
 
-	var $formplateEls = document.querySelectorAll($self.options.selector);
-	var $tester = tool.clone(document.querySelector($self.options.selector));
+	return Web;
+})(Web || {});
 
-	if (!tool.isTouch() && !tool.hasClass(tool.element.html, 'fp-no-touch')) {
-		tool.classAdd(tool.element.html, 'fp-no-touch');
-	}
+// Component container
+var Formplate = (function () {
+	// Defaults
+	var defaults = {
+		selector: '.formplate',
+		colour: 'blue',
+		label: 'normal',
+		style: 'line'
+	};
 
 	// Functions
-	var checkToggle = function($element) {
-		$element.onclick = function() {
-			if (tool.hasClass($element.parentNode, '_checked')) {
-				$element.checked = false;
-				tool.classRemove($element.parentNode, '_checked');
-			} else {
-				$element.checked = true;
-				tool.classAdd($element.parentNode, '_checked');
-			}
-		};
-	};
-	var inputContentFocus = function($thisFormEl) {
-		var $inputs = $thisFormEl.querySelectorAll('input');
-		for (var $i = 0, $len = $inputs.length; $i < $len; $i++) {
-			var $thisInput = $inputs[$i];
-			$thisInput.onfocus = function() {
-				var $parent = ($thisInput.parentNode.getAttribute('class').indexOf('fp-') > -1) ? $thisInput.parentNode : ($thisInput.parentNode.parentNode.getAttribute('class').indexOf('fp-') > -1) ? $thisInput.parentNode.parentNode : $thisInput.parentNode.parentNode.parentNode;
-				tool.classAdd($parent, '_focused _valued');
-			};
-			$thisInput.onblur = function() {
-				var $parent = ($thisInput.parentNode.getAttribute('class').indexOf('fp-') > -1) ? $thisInput.parentNode : ($thisInput.parentNode.parentNode.getAttribute('class').indexOf('fp-') > -1) ? $thisInput.parentNode.parentNode : $thisInput.parentNode.parentNode.parentNode;
-				tool.classRemove($parent, '_focused');
-				if ($thisInput.value.length < 1) {
-					tool.classRemove($parent, '_valued');
-				}
-			};
+	var setup = function () {
+		if (!Web.is.touch()) {
+			Web.class.add(Web.dom.html, 'fmp-no-touch');
 		}
 	};
-	var radioToggle = function($element) {
-		$element.onclick = function() {
-			var $inputRadioGroup = document.getElementsByName($element.getAttribute('name'));
-			for (var $i = 0, $len = $inputRadioGroup.length; $i < $len; $i++) {
-				$inputRadioGroup[$i].checked = false;
-				tool.classRemove($inputRadioGroup[$i].parentNode, '_checked');
-			}
-			$element.checked = true;
-			tool.classAdd($element.parentNode, '_checked');
-		};
-	};
-	var textareaContentFocus = function($textarea) {
-		$textarea.onfocus = function() {
-			tool.classAdd($textarea.parentNode, '_focused _valued');
-		};
-		$textarea.onblur = function() {
-			tool.classRemove($textarea.parentNode, '_focused');
-			if ($textarea.value.length < 1) {
-				tool.classRemove($textarea.parentNode, '_valued');
-			}
-		};
-	};
+	var setupFormElement = function (formContainer, options) {
+		// Variables
+		var element = false;
+		var classes = ['_c-' + options.colour, '_s-' + options.style];
+		var type = false;
 
-	// Loop over all elements and apply
-	var $globalClasses = ['_c-' + $self.options.colour, '_s-' + $self.options.style];
-	for (var $i = 0, $len = $formplateEls.length; $i < $len; $i++) {
-		var $thisFormEl = $formplateEls[$i];
-		var $classes = tool.clone($globalClasses);
-
-		// Set the input classes
-		if ($thisFormEl.querySelector('input')) {
-			var $input = $thisFormEl.querySelector('input');
-			var $inputType = $input.getAttribute('type');
-
-			if ($inputType === 'checkbox' || $inputType === 'radio') {
-				// Removed _checked if need be
-				if ($input.checked === true) {
-					$classes.push('_checked');
-				} else {
-					if (tool.hasClass($thisFormEl, '_checked')) {
-						tool.classRemove($thisFormEl, '_checked');
-					}
-				}
-				// Checkbox type
-				if (tool.hasClass($input, 'toggler')) {
-					$classes.push('fp-tog');
-				} else {
-					$classes.push('fp-check');
-					if ($inputType === 'radio') {
-						$classes.push('_t-radio');
-					}
-				}
-				tool.classAdd($thisFormEl, $classes);
-				// Add events
-				if ($inputType === 'checkbox') {
-					checkToggle($input);
-				} else if ($inputType === 'radio') {
-					radioToggle($input);
-				}
-			} else if ($inputType === 'password') {
-				if ($self.options.label.length > 0 && $self.options.label !== 'normal') {
-					$classes.push('_l-' + $self.options.label);
-				}
-				$classes.push('fp-inp', '_t-password');
-				if ($input.value.length > 0) {
-					$classes.push('_valued');
-				}
-				tool.classAdd($thisFormEl, $classes);
-				inputContentFocus($thisFormEl);
-			} else {
-				$classes.push('fp-inp');
-				if ($self.options.label.length > 0 && $self.options.label !== 'normal') {
-					$classes.push('_l-' + $self.options.label);
-				}
-				if ($input.value.length > 0) {
-					$classes.push('_valued');
-				}
-				tool.classAdd($thisFormEl, $classes);
-				inputContentFocus($thisFormEl);
-			}
-		} else if ($thisFormEl.querySelector('textarea')) {
-			var $textarea = $thisFormEl.querySelector('textarea');
-			if ($self.options.label.length > 0 && $self.options.label !== 'normal') {
-				$classes.push('_l-' + $self.options.label);
-			}
-			$classes.push('fp-text');
-			if ($textarea.value.length > 0) {
-				$classes.push('_valued');
-			}
-			tool.classAdd($thisFormEl, $classes);
-			textareaContentFocus($textarea);
-		} else if ($thisFormEl.querySelector('select')) {
-			var $select = $thisFormEl.querySelector('select');
-			if ($select != null) {
-				$classes.push('fp-sel');
-				tool.classAdd($thisFormEl, $classes);
-			}
+		// Set type
+		if (formContainer.querySelector('input')) {
+			// Inputs
+			element = formContainer.querySelector('input');
+			type = element.getAttribute('type');
+		} else if (formContainer.querySelector('textarea')) {
+			// Textarea
+			element = formContainer.querySelector('textarea');
+			type = 'textarea';
+		} else if (formContainer.querySelector('select')) {
+			// Selects
+			element = formContainer.querySelector('select');
+			type = 'select';
 		}
+
+		// Apply styles
+		switch (type) {
+			case 'checkbox':
+			case 'radio':
+				// Checked
+				if (element.checked) {
+					classes.push('_checked');
+				} else {
+					Web.class.remove(element, '_checked');
+				}
+				// Toggler?
+				if (Web.has.class(element, 'toggler')) {
+					classes.push('fmp-tog');
+				} else {
+					classes.push('fmp-check');
+					if (type === 'radio') {
+						classes.push('_t-radio');
+					}
+				}
+				break;
+
+			case 'text':
+			case 'password':
+				classes.push('fmp-inp');
+				if (type === 'password') {
+					classes.push('_t-password');
+				}
+				if (options.label.length > 0 && options.label !== 'normal') {
+					classes.push('_l-' + options.label);
+				}
+				if (element.value.length > 0) {
+					classes.push('_valued');
+				}
+				break;
+
+			case 'textarea':
+				if (options.label.length > 0 && options.label !== 'normal') {
+					classes.push('_l-' + options.label);
+				}
+				if (element.value.length > 0) {
+					classes.push('_valued');
+				}
+				break;
+
+			case 'select':
+				classes.push('fmp-sel');
+				break;
+
+		}
+		Web.class.add(formContainer, classes);
+
+		// Return
+		return {
+			container: formContainer,
+			element: element,
+			type: type
+		}
+	};
+
+	// Inner component
+	var component = function (objForm) {
+		console.log(objForm);
+	};
+
+	// Initialiser
+	var init = function (uOptions) {
+		// Options
+		var uOptions = (typeof uOptions === 'object') ? uOptions : false; // User options
+		var options = {
+			selector: Web.helper.setDefault(uOptions.selector, defaults.selector),
+			colour: Web.helper.setDefault(uOptions.colour, defaults.colour),
+			label: Web.helper.setDefault(uOptions.label, defaults.label),
+			style: Web.helper.setDefault(uOptions.style, defaults.style)
+		};
+		var formElements = document.querySelectorAll(options.selector);
+		// Catch
+		if (formElements.length < 1) {
+			return false;
+		}
+		// Initialise each component and return
+		var objReturn = [];
+		for (var i = 0, len = formElements.length; i < len; i++) {
+		   objReturn.push(new component(setupFormElement(formElements[i], options)));
+		}
+	};
+
+	// Execute and return
+	setup();
+	return {
+		defaults: defaults,
+		init: init
 	}
-};
+})();
