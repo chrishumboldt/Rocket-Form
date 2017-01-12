e89e8cf2230b3d134b52a4e83a957a21850ffff9
Chris Humboldt <chrismodem@gmail.com>
2017-01-12 11:42:10 +0200

    Code cleanup.

--- build/ts/form.ts
+++ build/ts/form.ts
@@ -70,49 +70,48 @@ module RockMod_Form {
       switch (type) {
          case 'checkbox':
          case 'radio':
-         // Checked
-         if (element.checked) {
-            classes.push('_checked');
-         } else {
-            Rocket.classes.remove(element, '_checked');
-         }
-         // Toggler?
-         if (Rocket.has.class(element, 'toggler')) {
-            classes.push('rf-tog');
-         } else {
-            classes.push('rf-check');
-            if (type === 'radio') {
-               classes.push('_t-radio');
+            // Checked
+            if (element.checked) {
+               classes.push('_checked');
+            } else {
+               Rocket.classes.remove(element, '_checked');
             }
-         }
-         break;
+            // Toggler?
+            if (Rocket.has.class(element, 'toggler')) {
+               classes.push('rf-tog');
+            } else {
+               classes.push('rf-check');
+               if (type === 'radio') {
+                  classes.push('_t-radio');
+               }
+            }
+            break;
 
          case 'textarea':
-         classes.push('rf-text');
-         if (options.label.length > 0 && options.label !== 'normal') {
-            classes.push('_l-' + options.label);
-         }
-         if (typeof element.value === 'string' && element.value.length > 0) {
-            classes.push('_valued');
-         }
-         break;
+            classes.push('rf-text');
+            if (options.label.length > 0 && options.label !== 'normal') {
+               classes.push('_l-' + options.label);
+            }
+            if (typeof element.value === 'string' && element.value.length > 0) {
+               classes.push('_valued');
+            }
+            break;
 
          case 'select':
-         classes.push('rf-sel');
-         break;
+            classes.push('rf-sel');
+            break;
 
          default:
-         classes.push('rf-inp');
-         if (type === 'password') {
-            classes.push('_t-password');
-         }
-         if (options.label.length > 0 && options.label !== 'normal') {
-            classes.push('_l-' + options.label);
-         }
-         if (typeof element.value === 'string' && element.value.length > 0) {
-            classes.push('_valued');
-         }
-         break;
+            classes.push('rf-inp');
+            if (type === 'password') {
+               classes.push('_t-password');
+            }
+            if (options.label.length > 0 && options.label !== 'normal') {
+               classes.push('_l-' + options.label);
+            }
+            if (typeof element.value === 'string' && element.value.length > 0) {
+               classes.push('_valued');
+            }
 
       }
       Rocket.classes.add(formContainer, classes);
@@ -128,36 +127,35 @@ module RockMod_Form {
       function toggle(option) {
          switch (type) {
             case 'checkbox':
-            checkSelect(option, type, element, formContainer);
-            break;
+               checkSelect(option, type, element, formContainer);
+               break;
             case 'radio':
-            radioSelect(option, type, element, formContainer);
-            break;
+               radioSelect(option, type, element, formContainer);
+               break;
          };
       }
 
       // Bind events
       switch (type) {
          case 'checkbox':
-         Rocket.event.add(element, 'click', function () {
-            checkSelect(false, type, element, formContainer);
-         });
-         break;
+            Rocket.event.add(element, 'click', function () {
+               checkSelect(false, type, element, formContainer);
+            });
+            break;
 
          case 'radio':
-         Rocket.event.add(element, 'click', function () {
-            radioSelect(false, type, element, formContainer);
-         });
-         break;
+            Rocket.event.add(element, 'click', function () {
+               radioSelect(false, type, element, formContainer);
+            });
+            break;
 
          default:
-         Rocket.event.add(element, 'focus', function () {
-            Rocket.classes.add(formContainer, ['_focused', '_valued']);
-         });
-         Rocket.event.add(element, 'blur', function () {
-            Rocket.classes.remove(formContainer, ['_focused'].concat((element.value.length < 1) ? ['_valued'] : []));
-         });
-         break;
+            Rocket.event.add(element, 'focus', function () {
+               Rocket.classes.add(formContainer, ['_focused', '_valued']);
+            });
+            Rocket.event.add(element, 'blur', function () {
+               Rocket.classes.remove(formContainer, ['_focused'].concat((element.value.length < 1) ? ['_valued'] : []));
+            });
       };
 
       // Return
